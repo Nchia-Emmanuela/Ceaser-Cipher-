7d29436ddaf820d3604c82724a777475b8449200
Nchia-Emmanuela <83321016+Nchia-Emmanuela@users.noreply.github.com>
2021-08-17 12:24:27 +0100

    prevent form submission

--- index.js
+++ index.js
@@ -21,3 +21,40 @@ decrypt.addEventListener('click', () => {
     encryptForm.style.display = 'none';
     decryptForm.style.display = 'block';
 });
+
+var key;
+
+function en_de_key(e) {
+    // console.log(e.target.value);
+    key = e.target.value;
+    // if (key == 0 || key < 0) {
+    //     e.target.value = 1;
+    // }
+}
+
+let message = "";
+
+function en_de_message(e) {
+    // console.log(e.target.value);
+    if (key === 0) {
+        e.target.value = "";
+    }
+}
+
+// let num = 8;
+// var letter = '';
+// letter = num;
+
+// console.log(type(num));
+// console.log(letter);
+
+const de_button = document.querySelector('.de-button');
+const en_button = document.querySelector('.en-button');
+
+en_button.addEventListener('click', (e) => {
+    e.preventDefault();
+});
+
+de_button.addEventListener('click', (e) => {
+    e.preventDefault();
+});
